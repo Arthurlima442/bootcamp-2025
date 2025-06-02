120b33a044ab52f20debed2503e856fa1ebfd704
Arthur Lima <arthurlima442@gmail.com>
2025-06-02 13:38:40 +0100

    adicionando files no git

---
+++ MY APP Films/MY APP Films/main.swift
@@ -0,0 +1,83 @@
+//
+//  main.swift
+//  MY APP Films
+//
+//  Created by Barbara Brigolin on 28/04/25.
+//
+
+
+//exemplos de nome: CineVibe,
+
+// Meu app de filmes
+
+//d88b4facc52e394846c3f340cfd88685
+
+// Por que app de filmes ?
+
+/// Eu como todas as pessoas gostamos muito de filmes, series e diversos outros fomatos, tanto novos ou velhos, com base na minha vida. Eu sempre gostei de filmes antigos e alguns grupos expecificos de filmes e series, entao assim que comecei minhas aulas de dev ios, pensei : irei fazer um app sobre, pois é algo que eu acho muito interresante e que ajuda muito quando uma pessoa ou um grupo de amigos querem ver algum filme mas ficam ek duvida ou ficam abrindo varios app de filmes para procurar qual assistir ou qual categoria tem aquele filme para a ocasiao, por isso eu decidi desenvolver um app sobre filmes, que vai ajudar as pessoas a achar aquele filme ideal para ver ou ate mesmo ver os lancamentos que estao por vim, ja que muitas pessoas estao sempre na correria nos trabalhos e nao tem tempo para esta sempre conectado e pesquisando em varios sites, meu app ira ser mais pratico e atender suas necessidades com muito menos esforço.
+
+
+// Mark : Mensagem de venda
+
+
+//Desde sempre, assim como muitas pessoas, sou apaixonado por filmes, séries e outros formatos audiovisuais, sejam lançamentos ou clássicos antigos. Durante minha experiência pessoal, percebi o quanto pode ser difícil escolher o que assistir, especialmente quando estamos em grupo ou com pouco tempo disponível. Muitas vezes, acabamos pulando de um aplicativo para outro tentando decidir qual filme ver — o que torna o momento de lazer cansativo em vez de prazeroso.
+
+//Foi com base nessa realidade que, ao iniciar minhas aulas de desenvolvimento iOS, decidi criar um aplicativo voltado para amantes do cinema. Minha proposta é simples, mas poderosa: um app prático, intuitivo e centralizado, que ajuda os usuários a encontrar rapidamente o filme ideal para qualquer ocasião, seja por categoria, popularidade ou lançamentos futuros.
+
+//Vivemos em um mundo acelerado, e nem sempre temos tempo para pesquisar em vários sites ou plataformas. Meu aplicativo resolve esse problema, oferecendo recomendações personalizadas, notificações de estreias e uma experiência organizada que facilita a vida do usuário. Acredito que essa solução tem potencial real de mercado e pode se tornar um diferencial para quem busca praticidade e qualidade em entretenimento. (somente filmes neste primeiro momento)
+
+//Por isso, acredito que vale a pena investir nesta ideia.
+
+
+// primeira coisa no app:
+
+// tela de login/registrar sua conta
+
+/// Detalhes
+/// -> login: normalmente apenas 1 campo de texto para senha, 1 para email, 1 botão "logar", e se quiser mais opções-- logar com facebook, google ou apple
+/// ->Register: nome, email, senha, confirmar senha, botão cadastrar, (ou botoes de registro com facebook, google ou apple)
+/// para tornar esse login e registro possível usarei uma Ferramenta chamada Firebase.
+
+/// Ao logar no App voce será direcionado logo de inicio para a tela Home
+
+// Tela Inicial (Home): - nome da tela "Filmes"
+
+  ///Exibe uma lista de filmes recomendados aleatoriamente
+  /// uma barra de pesquisa
+  ///Nessa primeira tela você ira ter alguns grupos de filmes que serão recomendados a você, de forma aleatoria, junto a barra de pesquisa que estara na tela inicial.
+ ///Nesta tela terá um filtro, nele tera as categorias: ação, animação, aventura, comedia...
+// Barra de pesquisa: tela inicial
+   
+   /// A barra de pesquisa ira está no todo da tela home, quando o usuario clicar nela, ele ira escrever o nome do seu Filme de escolha para que possa busca na base de dados e dar a resposta ao usuario.
+   /// O Filme que irá mostra na tela, sera seu filme de  sua escolha.
+
+// Quando clicar em algum Filme de sua escolha: tela inicial
+
+   ///Mostra informações completas sobre o filme selecionado.
+  /// EX:  Nome do filme,  Data de lançmento,  Categoria, SINOPSE,  Ano de lançamento, algumas imagens do Filme
+///  add favaritos,
+
+
+
+// clicando na ultimo icone na parte inferior:
+//icone - (minha conta): tab bar ultimo
+// abrirá a tela minha conta
+//primeiro na parte superior: foto de perfil de forma opcional, Nome - ao lado icone de engrenagem.
+/// Dentro da engrenagem mostrará as seguintes informações:
+/// nome
+/// email
+/// data de nascimento
+// final da tela tera um botao de "SAIR" que ira te deslogar do app
+
+  ///Está tela ira infomar seu dados como: email, nome e data de nascimento.
+  ///Nesta tela ira ter a aba de filmes favoritos, onde esta disponivel ao usuario, contendo todo aquele filme que ele selecionou..
+
+
+
+
+
+
+
+
+
+
---
+++ MeuPrimeiroApp Arthur L/MeuPrimeiroApp Arthur L/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeuPrimeiroApp Arthur L
+//
+//  Created by Barbara Brigolin on 13/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
