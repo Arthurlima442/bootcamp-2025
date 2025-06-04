df8e2e9c7b4d4f62d6bdebe4869cab6c9dc64d02
Arthur Lima <arthurlima442@gmail.com>
2025-06-04 16:42:14 +0100

    Revert "no message"
    This reverts commit d967493df29da64d893d22a367ba95e195754d54.

---
+++ MeuPrimeiroApp Arthur L/MeuPrimeiroApp Arthur LUITests/MeuPrimeiroApp_Arthur_LUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  MeuPrimeiroApp_Arthur_LUITestsLaunchTests.swift
+//  MeuPrimeiroApp Arthur LUITests
+//
+//  Created by Barbara Brigolin on 13/04/25.
+//
+
+import XCTest
+
+final class MeuPrimeiroApp_Arthur_LUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
