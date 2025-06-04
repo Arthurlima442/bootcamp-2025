3e2b7d1bda2426239dd100ebd2af5551ed6fcbeb
Arthur Lima <arthurlima442@gmail.com>
2025-06-04 23:01:23 +0100

    atualiazao tela login

--- BootCamp Arthur/PrimeiroAppViewCode2-Arthur/PrimeiroAppViewCode2-Arthur/Login/Screen/LoginScreen.swift
+++ BootCamp Arthur/PrimeiroAppViewCode2-Arthur/PrimeiroAppViewCode2-Arthur/Login/Screen/LoginScreen.swift
@@ -19,12 +19,30 @@ class LoginScreen: UIView {
         return label
     }()
     
+    lazy var logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(systemName: "person.fill")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    lazy var emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Digite seu email:"
+        tf.borderStyle = .roundedRect
+        tf.keyboardType = .emailAddress
+        
+        return tf
+    }()
+    
     
     
     
     init() {
         super.init(frame: .zero)
-        backgroundColor = .orange
+        backgroundColor = .lightGray
         addElements()
         configContraints()
         
@@ -38,15 +56,26 @@ class LoginScreen: UIView {
     
     func addElements() {
         addSubview(loginLabel)
+        addSubview(logoImageView)
+        addSubview(emailTextField)
     }
     
     func configContraints() {
         NSLayoutConstraint.activate([
             loginLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
-            loginLabel.centerXAnchor.constraint(equalTo: centerXAnchor)
+            loginLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            logoImageView.topAnchor.constraint(equalTo: loginLabel.bottomAnchor, constant: 20),
+            logoImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 100),
+            logoImageView.widthAnchor.constraint(equalToConstant: 100),
+
+            emailTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 50),
+            emailTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            emailTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            emailTextField.heightAnchor.constraint(equalToConstant: 40),
+
+            
         ])
     }
-    
-    
-    
 }
